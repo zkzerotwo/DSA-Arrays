759547b3339637b9d4d9f1c34341ec4bcaccaedc
Christopher 'ZK' Hal <zkzerotwo@gmail.com>
2021-04-21 16:14:58 -0500

    working through drills

--- array.js
+++ array.js
@@ -1,25 +1,7 @@
-const memory = require('./memory')
+// const Memory = require('../DSA-Big-  O/memory');
+const Memory = require('./memory')
 
-// class Array {
-//     constructor() {
-//         this.length = 0;
-//         this.ptr = memory.allocate(this.length);
-//     }
-//     push(value) {
-//         this._resize(this.length + 1);
-//         memory.set(this.ptr + this.length, value);
-//         this.length++;
-//     }
-//     _resize(size) {
-//         const oldPtr = this.ptr;
-//         this.ptr = memory.allocate(size);
-//         if (this.ptr === null) {
-//             throw new Error('Out of memory');
-//         }
-//         memory.copy(this.ptr, oldPtr, this.length);
-//         memory.free(oldPtr);
-//     }
-// }
+let memory = new Memory()
 
 class Array {
     constructor() {
